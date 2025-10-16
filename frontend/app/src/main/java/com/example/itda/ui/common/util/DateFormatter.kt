860b71adcb2f3894ab4b82e39794bb77c7b8de6e
jm020827 <jmsmlove02@snu.ac.kr>
2025-10-17 02:44:22 +0900

    feat(frontend) #30 : implement date related util functions

--- frontend/app/src/main/java/com/example/itda/ui/common/util/DateFormatter.kt
+++ frontend/app/src/main/java/com/example/itda/ui/common/util/DateFormatter.kt
@@ -1,2 +1,21 @@
 package com.example.itda.ui.common.util
 
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun getTodayString(): String {
+    val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    return sdf.format(Date())
+}
+
+fun getDDayLabel(endDate: String): Long {
+    val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+
+    val today = Calendar.getInstance()
+    val end = Calendar.getInstance().apply {
+        time = sdf.parse(endDate) ?: Date()
+    }
+
+    val diff = (end.timeInMillis - today.timeInMillis) / (1000 * 60 * 60 * 24)
+    return diff
+}
