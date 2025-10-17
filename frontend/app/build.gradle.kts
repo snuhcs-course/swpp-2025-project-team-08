76341f047ff33b540e6c6d6a2cb28a3b4616e55e
enggaa <enggaa@snu.ac.kr>
2025-10-17 15:01:34 +0900

    feat: implement base Low-Level Component

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.navigation.common.ktx)
+    implementation(libs.androidx.compose.material.icons.extended)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
