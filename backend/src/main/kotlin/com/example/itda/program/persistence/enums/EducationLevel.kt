3d1895986117d91e192f163d200742316bd90e65
wonseok <wonseok1122@gmail.com>
2025-10-23 19:33:39 +0900

    be(fix) #52 : Specify educationLevel enum

--- backend/src/main/kotlin/com/example/itda/program/persistence/enums/EducationLevel.kt
+++ backend/src/main/kotlin/com/example/itda/program/persistence/enums/EducationLevel.kt
@@ -3,8 +3,11 @@ package com.example.itda.program.persistence.enums
 enum class EducationLevel(val dbValue: String) {
     ANY("무관"),
     HIGHSCHOOL("고졸"),
+    STUDENT("재학생"),
+    LEAVE_OF_ABSENCE("휴학생"),
+    EXPECTED_GRADUATE("졸업예정"),
     ASSOCIATE("전문대졸"),
-    BACHELOR("학사"),
+    BACHELOR("대졸"),
     MASTER("석사"),
     PHD("박사"),
     ;
