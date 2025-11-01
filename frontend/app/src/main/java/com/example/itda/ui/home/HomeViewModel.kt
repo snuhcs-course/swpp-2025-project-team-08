55163b0b4118c6c929a7409410cc49b7a3041642
jm020827 <jmsmlove02@snu.ac.kr>
2025-11-01 23:50:11 +0900

    feat(fe) #44 : implement loading

--- frontend/app/src/main/java/com/example/itda/ui/home/HomeViewModel.kt
+++ frontend/app/src/main/java/com/example/itda/ui/home/HomeViewModel.kt
@@ -10,12 +10,12 @@ import com.example.itda.data.repository.ProgramRepository
 import com.example.itda.data.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jakarta.inject.Inject
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-
 
 
 @HiltViewModel
@@ -29,20 +29,37 @@ class HomeViewModel @Inject constructor(
         val categories: List<Category> = DummyData.dummyCategories, // 필터 카테고리
         val selectedCategoryName: String = DummyData.dummyCategories.first().name, // 선택된 카테고리
         val feedItems: List<FeedItem> = emptyList(), // 메인 피드 목록 (ProgramRepository에서 가져올 데이터)
-        val isLoading: Boolean = false
+        val isLoading: Boolean = true,
+        val isRefreshing: Boolean = false
     )
+
+
 
     private val _homeUi = MutableStateFlow(HomeUiState())
     val homeUi: StateFlow<HomeUiState> = _homeUi.asStateFlow()
 
     init {
-        loadHomeData()
+        viewModelScope.launch {
+
+            loadHomeData()
+
+        }
+    }
+
+    fun refreshHomeData() {
+        viewModelScope.launch {
+            _homeUi.update { it.copy(isRefreshing = true) }
+            loadHomeData() // TODO - 그냥 load 가 아니라 메트릭을 조정해서 더 나은 결과 / 더 넓은 추천범위로 load 하는 방식 고민중
+            _homeUi.update { it.copy(isRefreshing = false) }
+        }
     }
 
     // 홈 화면에 필요한 모든 데이터를 로드
     private fun loadHomeData() {
         viewModelScope.launch {
             _homeUi.update { it.copy(isLoading = true) }
+
+            delay(2000L) // TODO - 실제 통신에서는 실제 딜레이가 있을거라 빼도 됨
 
             // 1. 피드 데이터 로드
             val programs = // TODO: programRepository.getPrograms() 구현 후 호출
@@ -56,7 +73,8 @@ class HomeViewModel @Inject constructor(
                 it.copy(
                     user = user,
                     feedItems = programs,
-                    isLoading = false
+                    isLoading = false,
+                    isRefreshing = false
                 )
             }
         }
