f920b24980b1843a9f02734a35aa7f9109ced26b
honeyyyoon <147039633+honeyyyoon@users.noreply.github.com>
2025-11-11 13:45:52 +0900

    refactor(frontend) #124: additional-feature-at-auth

--- frontend/app/src/main/java/com/example/itda/ui/search/components/SearchFilterComponents.kt
+++ frontend/app/src/main/java/com/example/itda/ui/search/components/SearchFilterComponents.kt
@@ -3,7 +3,9 @@ package com.example.itda.ui.search.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
